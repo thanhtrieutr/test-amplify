40fb2a4bc24d1edf71e4f3e0106f41922e42f1f9
Thanh <trieuthanhtr@gmail.com>
2019-08-29 13:28:34 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Amplify, { Analytics, Storage, API } from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react'; // or 'aws-amplify-react-native';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+Amplify.configure(awsconfig);
+
+
+class App extends React.Component {
+  post = async () => {
+    console.log('calling api');
+    const response = await API.post('testApi', '/items', {
+      body: {
+        id: '3',
+        name: 'Thanh Trieu Tran hello amplify!',
+        helloId: 'alo alo'
+      }
+    });
+    alert(JSON.stringify(response, null, 2));
+  }
+  get = async () => {
+    console.log('calling api');
+    const response = await API.get('testApi', '/items/object/3');
+    alert(JSON.stringify(response, null, 2));
+  }
+  list = async () => {
+    console.log('calling api');
+    const response = await API.get('testApi', '/items');
+    alert(JSON.stringify(response, null, 2));
+  }
+  render() {
+    return (
+      <div className="App">
+        <p> Pick a file</p>
+        <input type="file" onChange={this.uploadFile} />
+        <button onClick={this.post}>POST</button>
+        <button onClick={this.get}>GET</button>
+        <button onClick={this.list}>LIST</button>
+        
+      </div>
+    );
+  }
 }
 
-export default App;
+// export default App;
+export default withAuthenticator(App, true);
