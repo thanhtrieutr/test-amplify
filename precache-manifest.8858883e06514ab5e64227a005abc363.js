0d27b4d5976d4f62a7754df85a3b43ee13c12052
Thanh <trieuthanhtr@gmail.com>
2019-08-29 16:19:04 +0700

    Updates

---
+++ precache-manifest.8858883e06514ab5e64227a005abc363.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e9185c0957e64c4f793659c660fa00",
+    "url": "/test-amplify/index.html"
+  },
+  {
+    "revision": "84072cfc307d3af9b47f",
+    "url": "/test-amplify/static/css/2.eee0f4a3.chunk.css"
+  },
+  {
+    "revision": "de315ebda020bbe38b83",
+    "url": "/test-amplify/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "84072cfc307d3af9b47f",
+    "url": "/test-amplify/static/js/2.741b91b1.chunk.js"
+  },
+  {
+    "revision": "de315ebda020bbe38b83",
+    "url": "/test-amplify/static/js/main.8a61a4bb.chunk.js"
+  },
+  {
+    "revision": "fd1cd04b560812cdf1b1",
+    "url": "/test-amplify/static/js/runtime~main.e2dc6aea.js"
+  }
+]);
